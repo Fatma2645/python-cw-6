e96eee8ffe377bd949b3e293bbbae9c3e4b49aab
Fatma2645 <142684037+Fatma2645@users.noreply.github.com>
2023-09-06 13:48:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,31 @@
 person={
 "name":"fatma",
 "age":15,
-"hoppies":"horse rider"
+"hobbies":["horse rider","painter"]
 }
 
 
 
-print(person.get['name'])
+print(person.get ("name"))
 
 
-print(person.get['age'])
+print(person.get('age'))
 
 
 person["age"]=18
 person["country"]="Spain" 
-
 print(person)
+print("length of dictionary:",len(person))
+
+
+
+
+person["hobbies"].append("wriring")
+
+
+
+
+def check_hobbies(person):
+    if len (person['hobbies'])>3:
+        print("wow you are amazing")
+check_hobbies(person)     

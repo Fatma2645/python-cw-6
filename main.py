00132e83e5e4cfedb066a1651137d5ea42c73daa
Fatma2645 <142684037+Fatma2645@users.noreply.github.com>
2023-09-06 12:27:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # write your code here
+person={
+"name":"fatma",
+"age":15,
+"hoppies":"horse rider"
+}
+
+
+
+print(person.get['name'])
+
+
+print(person.get['age'])
+
+
+person["age"]=18
+person["country"]="Spain" 
+
+print(person)
